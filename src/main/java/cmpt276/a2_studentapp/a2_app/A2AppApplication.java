1927ad73fd0646ab18e628039f6083fcdfeb5cc3
Kelly <kellyk2603@gmail.com>
2024-02-25 00:33:05 -0800

    initial push

---
+++ src/main/java/cmpt276/a2_studentapp/a2_app/A2AppApplication.java
@@ -0,0 +1,13 @@
+package cmpt276.a2_studentapp.a2_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A2AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmpt276/a2_studentapp/a2_app/A2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpt276.a2_studentapp.a2_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
